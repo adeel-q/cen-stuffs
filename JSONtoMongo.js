1a102b30033b6f401e76d34e02d6102c3498b1a4
adeel qureshi <qureshia@ufl.edu>
2018-09-21 14:13:20 -0400

    assignment3 works

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,33 +1,64 @@
-'use strict';
+"use strict";
 /* 
   Import modules/files you may need to correctly run the script. 
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
-var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+var fs = require("fs"),
+  mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  Listing = require("./ListingSchema.js"),
+  config = require("./config");
 
 /* Connect to your database */
-mongoose.connect(config.db.uri);
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
- */
-var data = fs.readFileSync('listings.json').toString();
-var listingData = JSON.parse(data);
+mongoose.connect(
+  config.db.uri,
+  { useNewUrlParser: true }
+);
 
-/* Go through all the listings.json objects and create a mongo model objects and save them */
-for(var i=0; i<listingData.entries.length; i++) {
+var db = mongoose.connection; // Get a handle to the database connection object
+db.on("error", console.error.bind(console, "connection error:")); // From mongoose documentation
+
+// use db.once('open')
+db.once("open", function() {
+  /* 
+      Instantiate a mongoose model for each listing object in the JSON file, 
+      and then save it to your Mongo database 
+    */
+  var data = fs.readFileSync("listings.json").toString();
+  var listingData = JSON.parse(data);
+
+  /* Create an array of Promise objects to store each "save" promise */
+  var promisesArr = [];
+
+  /* Go through all the listings.json objects and create mongo model objects and save them */
+  for (var i = 0; i < listingData.entries.length; i++) {
     var newEntry = new Listing(listingData.entries[i]);
-    console.log(newEntry);
-    newEntry.save(function(err) {
-        if (err) throw err;
-        console.log('entry added');
+
+    //console.log(newEntry);
+
+    var entryPromise = newEntry
+      .save()
+      .then(function callback(entry) {
+        console.log("Saved entry " + entry.name);
+      })
+      .catch(function err_callback(err) {
+        console.log(err);
+      });
+    promisesArr.push(entryPromise);
+  }
+
+  /* Once all promises are resolved, close mongoose connection */
+  Promise.all(promisesArr)
+    .then(function(test) {
+      console.log("Finished saving all entries.");
+      mongoose.connection.close();
+    })
+    .catch(function err_callback2(err) {
+      console.log(err);
     });
-};
-console.log('done');
+});
+
+console.log("End of script."); // Will be run async.
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,21 +1,23 @@
 /* Fill out these functions using Mongoose queries*/
-var Listing = require('./ListingSchema.js'),
-    mongoose = require('mongoose'),
-    config = require('./config.js');
-
+var Listing = require("./ListingSchema.js"),
+  mongoose = require("mongoose"),
+  config = require("./config.js");
 
 /* Connect to your database */
-mongoose.connect(config.db.uri);
+mongoose.connect(
+  config.db.uri,
+  { useNewUrlParser: true }
+);
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
-    Listing.find({ name: 'Library West' }, function (err, listing) {
-        if (err) throw err;
-        console.log(listing);
-    })
+  Listing.find({ name: "Library West" }, function(err, listing) {
+    if (err) throw err;
+    console.log(listing);
+  });
 };
 var removeCable = function() {
   /*
@@ -23,48 +25,53 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
-    Listing.find({ code: 'CABL' }, function (err, listing) {
-        if (err) throw err;
-        console.log(listing);
-        /*listing.remove(function (err) {
+  Listing.find({ code: "CABL" }, function(err, listing) {
+    if (err) throw err;
+    console.log(listing);
+    /*listing.remove(function (err) {
             if (err) throw err;
             console.log('Entry deleted');
         });*/
-    });
-    Listing.remove({ code: 'CABL' }, function (err) {
-        if (err) throw err;
-    });
-    console.log('entry removed');
+  });
+  Listing.deleteOne({ code: "CABL" }, function(err) {
+    if (err) throw err;
+  });
+  console.log("entry removed");
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
-    Listing.find({code: 'PHL' }, function (err, listings) {
+  Listing.find({ code: "PHL" }, function(err, listings) {
+    if (err) throw err;
+    for (var i = 0; i < listings.length; i++) {
+      listings[i].address =
+        "100 Phelps Lab, Gainesville, FL 32603, United States";
+      listings[i].save(function(err) {
         if (err) throw err;
-        for (var i = 0; i < listings.length; i++) {
-            listings[i].address = '100 Phelps Lab, Gainesville, FL 32603, United States';
-            listings[i].save(function (err) {
-                if (err) throw err;
-                console.log('entry updated');
-            });
-        };
-        console.log(listings);
-    });
+        console.log("entry updated");
+      });
+    }
+    //console.log(listings);
+  });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
-    Listing.find({}, function (err, listing) {
-        if (err) throw err;
-        console.log(listing);
-    });
+  Listing.find({}, function(err, listing) {
+    if (err) throw err;
+    console.log(listing);
+  });
 };
 
-findLibraryWest();
+//findLibraryWest();
 removeCable();
-retrieveAllListings();
-updatePhelpsLab();
+//retrieveAllListings();
+//updatePhelpsLab();
 
+//mongoose.connection.close(); // needs to wait before closing connection, give it 1sec
+setTimeout(function() {
+  mongoose.connection.close();
+}, 1000);
