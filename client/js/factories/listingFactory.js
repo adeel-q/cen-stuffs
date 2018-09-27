d343f9a95bad0a2ca1bf22f93db464682ca3b2ed
root <root@DESKTOP-KAMOIHD.localdomain>
2018-09-27 16:40:38 -0400

    heroku specific changes

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,20 @@
 angular.module("listings", []).factory("Listings", function($http) {
+  var hostname = "https://blackmist-1.herokuapp.com"; //"http://localhost:8080";
+
   var methods = {
     getAll: function() {
-      return $http.get("http://localhost:8080/api/listings");
+      return $http.get(hostname + "/api/listings");
     },
 
     create: function(listing) {
-      return $http.post("http://localhost:8080/api/listings", listing);
+      return $http.post(hostname + "/api/listings", listing);
     },
 
     delete: function(id) {
       /**TODO
         return result of HTTP delete method
        */
-      var uri = "http://localhost:8080/api/listings/" + id;
+      var uri = hostname + "/api/listings/" + id;
       console.log("http://localhost:8080/api/listings/" + id);
 
       return $http.delete(uri);
